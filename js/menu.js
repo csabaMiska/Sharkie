092b049ca27d196a501b3d038257460a6d5576b5
MiskaCs90 <162002700+MiskaCs90@users.noreply.github.com>
2024-07-03 16:06:09 +0200

    Impressum Created

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
 let leaderBoard = document.getElementById('leaderBoard');
 let gameOptions = document.getElementById('gameOptions');
 let gameInstructions = document.getElementById('gameInstructions');
+let impressum = document.getElementById('impressum');
 
 function showGameOptions() {
     leaderBoard.classList.add('d-none');
     gameInstructions.classList.add('d-none');
+    impressum.classList.add('d-none');
     gameOptions.classList.remove('d-none');
 }
 
@@ -16,10 +18,23 @@ function hideGameOptions() {
 function showGameInstructions() {
     leaderBoard.classList.add('d-none');
     gameOptions.classList.add('d-none');
+    impressum.classList.add('d-none');
     gameInstructions.classList.remove('d-none');
 }
 
 function hideGameInstructions() {
     leaderBoard.classList.remove('d-none');
     gameInstructions.classList.add('d-none');
+}
+
+function showGameImpressum() {
+    leaderBoard.classList.add('d-none');
+    gameOptions.classList.add('d-none');
+    gameInstructions.classList.add('d-none');
+    impressum.classList.remove('d-none');
+}
+
+function hideGameImpressum() {
+    leaderBoard.classList.remove('d-none');
+    impressum.classList.add('d-none');
 }
